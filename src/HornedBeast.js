6b0f732c1312cf8a03c0dc2bb4a37c6c1a06f1ff
Lacey Washington <lacey.m.washington@gmail.com>
2023-04-19 16:37:59 -0500

    made the like but and added state

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,29 @@
 import React from "react";
 
-class HornedBeast extends React.Component{
-    render(){
-        return(
+class HornedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            likes: 0
+        }
+    }
+
+    incrementLike = () => {
+        this.setState({
+            likes: this.state.likes + 1
+        })
+    }
+    
+    render() {
+        return (
             <>
-            <h2>{this.props.title}</h2>
-            <img src={this.props.image_url} alt={this.props.title} height={250}/>
-            <p>{this.props.description}</p>
+                <h2>{this.props.title}</h2>
+                <img src={this.props.image_url} alt={this.props.title} height={250} />
+                <p>{this.props.description}</p>
+                <div>
+                    <p>{this.state.likes} Likes</p>
+                    <button onClick={this.incrementLike}>Like</button>
+                </div>
             </>
         )
     }
--- src/Main.js
+++ src/Main.js
@@ -1,44 +1,17 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
-const data = [{
-    "_id": 1,
-    "image_url": "https://www.scuba.com/blog/wp-content/uploads/2013/11/narwhal-rendering-underwater-feature.jpg",
-    "title": "NarWhal",
-    "description": "just the unicorn of the sea living its best life",
-    "keyword": "narwhal",
-    "horns": 1
-  },
-
-  {
-    "_id": 2,
-    "image_url": "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80",
-    "title": "Rhino Family",
-    "description": "Parent rhino with two babies",
-    "keyword": "rhino",
-    "horns": 2
-  },
-
-  {
-    "_id": 3,
-    "image_url": "https://previews.123rf.com/images/vadimone/vadimone1008/vadimone100800004/7517648-old-nubian-ibex-closeup.jpg",
-    "title": "Nubian Ibex",
-    "description": "What the hell you looking at?",
-    "keyword": "Ibex",
-    "horns": 2
-}];
+import data from "./data.json"
 
 class Main extends React.Component{
     render(){
-    const theBeast = [];
-    
-    data.forEach(beast =>{
-        theBeast.push(
+    const theBeast = data.map(beast =>
             <HornedBeast
             title ={beast.title}
             image_url={beast.image_url}
             description={beast.description} 
             />)
-    })
+        
+        
         return(
             <>
             <h2>Fantastic Horned Beasts</h2>
